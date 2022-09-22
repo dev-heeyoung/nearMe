b5685c6cfc3c8ad5e7ad89914673fd354d3163bf
Hyunjung Kim <hkim346@myseneca.ca>
2022-09-22 14:48:06 -0400

    index map updates

--- components/Map.jsx
+++ components/Map.jsx
@@ -7,64 +7,98 @@ import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source'; 
 import { Draw } from 'ol/interaction';
 import Feature from 'ol/Feature';
-import { Point } from 'ol/geom';
+import { MultiPoint, Point} from 'ol/geom';
+import { Icon} from 'ol/style';
 
 // import Event objects
 import Event from './Event'
 
+
 function Map(props){
 
-  const [mapObject, setMapObject] = useState({})  
+  const [mapObject, setMapObject] = useState({})
    useEffect(() => {
-
-        const rasterLayer =  new TileLayer({
-            source: new OSM()
-        })
-        const view = new View({
-            center:transform([-79.34851251318761, 43.796259859304776],'EPSG:4326' ,'EPSG:3857'),
-            zoom: 15
-        })
-
-        const pointFeature = new Feature({
-            geometry : new Point(transform([-79.34851251318761, 43.796259859304776],'EPSG:4326' ,'EPSG:3857'))
-        });
+        /* 
         
-        
-        const vectoerSource = new VectorSource({
-            features : [pointFeature]
+        // old code
+        //const eventFeatures = [];
+        const eventsCoord = [];
+        props.events.map((event)=>{ 
+            const eventFeature =  new Feature({
+                geometry : new Point(transform([event.longitude, event.latitude], 'EPSG:4326' ,'EPSG:3857')),
+                name : event.id
+            }); 
+            eventFeatures.push(eventFeature.getGeometry());
+            //console.log(eventFeature.getGeometry().getCoordinates());
         });
-        const vectorLayer = new VectorLayer({
-            source: vectoerSource,
+
+        console.log(eventFeatures);
+        //feature
+        const eventsFeatures = new Feature({
+            geometry : new MultiPoint(eventsCoord)
+        });
+
+        */
+      
+    //vector source for events
+        const eventSource = new VectorSource({});
+
+
+        props.events.map((event) =>{
+            const eventFeature =  new Feature({
+                geometry : new Point(transform([event.longitude, event.latitude], 'EPSG:4326' ,'EPSG:3857')),
+            }); 
+            eventFeature.setProperties({'eventId' : event.id})
+            eventSource.addFeature(eventFeature);
+            
+           // console.log("event id" +  eventFeature.getProperties() + "      event coodinate   " + transform([event.longitude, event.latitude], 'EPSG:4326' ,'EPSG:3857'));
+        })
+              
+
+    //vector layer for events
+        const eventLayer = new VectorLayer({
+            source: eventSource,
             style: {
-                'fill-color': 'rgba(255, 255, 255, 0.2)',
-                'stroke-color': '#ffcc33',
-                'stroke-width': 2,
-                'circle-radius': 7,
-                'circle-fill-color': '#ffcc33',
-            },
-        });
+                'circle-radius': 9,
+                'circle-fill-color': 'red'
+                },
+                zindex: 10
+            });
+
 
         const map = new OlMap({
-            layers: [rasterLayer, vectorLayer],
+            layers: [
+                new TileLayer({source: new OSM()}), 
+                new VectorLayer({source: new VectorSource()})
+            ],
             target: "map", 
-            view: view,
-          
+            view: new View({
+                center:transform([-79.34851251318761, 43.796259859304776],'EPSG:4326' ,'EPSG:3857'),
+                zoom: 15,
+            })
         })
+
         // click -> get lan.lon
+        /*
         const draw = new Draw({
-            //source: vectoerSource,
+            source: new VectorSource({wrapX: false}),
             type: 'Point',
           });
-
         map.addInteraction(draw);
-        map.addLayer(markerLayer);
+        */
+        map.addLayer(eventLayer);
+    
         map.on('click', function(evt){
-            console.log(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
+           map.forEachFeatureAtPixel(evt.pixel, function(feature){
+                   //console.log(feature.getGeometry().getFlatCoordinates());
+                   //console.log(feature.getProperties().eventId)
+                   console.log(props.events.find(event => event.id == feature.getProperties().eventId));
+                })
+            //console.log(transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
         })
         //
 
-        // display point 
-        console.log(props.events);
+        // display point
         setMapObject({ map })
         return ()=> null
     }, [])
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,36 +10,36 @@ export default function Home() {
   const events = [
     {
       id: 1,
-      title: "event1",
+      title: "Soul Cafe",
       description: "description",
       startTime: "2022-09-24 00:00:00",
       endTime: "2022-09-24 00:00:00",
-      latitude: 1.1,
-      longitude: 1.1,
+      latitude: 43.78682775424458,
+      longitude: -79.41771376155737,
       capacity: 5,
       participants: 3,
       availability: false,
     },
     {
       id: 2,
-      title: "event2",
+      title: "Finch Station",
       description: "description",
       startTime: "2022-09-24 00:00:00",
       endTime: "2022-09-24 00:00:00",
-      latitude: 1.1,
-      longitude: 1.1,
+      latitude: 43.780300572228875,
+      longitude: -79.41585241539359,
       capacity: 5,
       participants: 3,
       availability: false,
     },
     {
       id: 3,
-      title: "event3",
+      title: "Seneca College",
       description: "description",
       startTime: "2022-09-24 00:00:00",
       endTime: "2022-09-24 00:00:00",
-      latitude: 1.1,
-      longitude: 1.1,
+      latitude: 43.796259859304776,
+      longitude: -79.34851251318761,
       capacity: 5,
       participants: 3,
       availability: false,
@@ -57,7 +57,7 @@ export default function Home() {
         <FilterList />
         <div className="flex mx-auto">
           <Events events={events}/>
-          <Map />
+          <Map events={events} />
         </div>
       </main>
       <footer className="">
